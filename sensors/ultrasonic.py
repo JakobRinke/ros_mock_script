b4adfbd076c1cea0f4304b043ae401b4dfed1df8
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-05-15 21:20:47 +0200

    Update ultrasonic.py

--- sensors/ultrasonic.py
+++ sensors/ultrasonic.py
@@ -14,30 +14,36 @@ def ultrasonic_cal():
     GPIO.output(TRIG, GPIO.LOW)
     time.sleep(2)
 
-
 def ultrasonic():
 
+    GPIO.setmode(GPIO.BOARD)
+    
     TRIG = 18
     ECHO = 16
+    
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
+    
+    GPIO.output(TRIG, GPIO.LOW)
 
     try:
-            GPIO.output(TRIG, GPIO.HIGH)
-            time.sleep(0.00001)
-            GPIO.output(TRIG, GPIO.LOW)
+        GPIO.output(TRIG, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, GPIO.LOW)
 
-            while GPIO.input(ECHO)==0:
-                    pulse_start = time.time()
+        while GPIO.input(ECHO)==0:
+                pulse_start = time.time()
 
-            while GPIO.input(ECHO)==1:
-                    pulse_end = time.time()
+        while GPIO.input(ECHO)==1:
+                pulse_end = time.time()
 
-            pulse_duration = pulse_end-pulse_start
+        pulse_duration = pulse_end-pulse_start
 
-            distance = pulse_duration * 17150
+        distance = pulse_duration * 17150
 
-            distance = round(distance,2)
+        distance = round(distance,2)
 
     finally:
-            GPIO.cleanup()
+        GPIO.cleanup()
 
     return distance
